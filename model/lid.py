a44273789eef6bc84e72ad0aefa4a8379a5ebf19
maximetacquet <maxime.tacquet@student.vives.be>
2025-12-28 17:20:18 +0100

    Lid klasse bijgewerkt met controle op afdeling
    Als afdeling kan je enkel maar Pimpel, Speelclub, Rakwi, Tito, Keti en Aspi meegeven (niet hoofdlettergevoelig). Wanneer je een afdeling meegeeft die niet in de lijst van afdelingen staat zal de afdeling op "" gezet worden

--- model/lid.py
+++ model/lid.py
@@ -5,6 +5,8 @@ Created on Sun Dec 28 16:09:52 2025
 @author: maxime
 """
 
+Afdelingen = ["Pimpel","Speelclub","Rakwi","Tito","Keti","Aspi"]
+
 class Lid:
     def __init__(self, id: int=0, voornaam:str="", achternaam:str="", afdeling:str=""):
         if id is None:
@@ -21,9 +23,12 @@ class Lid:
             self.achternaam=achternaam.title()
         if afdeling is None:
             self.afdeling=""
+        elif afdeling.title() not in Afdelingen:
+            self.afdeling=""
+            print(f"Afdeling {afdeling} is ongeldig. Kies uit: {', '.join(Afdelingen)}")
+            print("Als afdeling werd er ipv een foute afdeling niets ingegeven")
         else:
             self.afdeling= afdeling.title()
     
     def __str__(self):
         return f"{self.voornaam} {self.achternaam} {self.afdeling}"
-  
