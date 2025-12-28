a1bdd00d9dfae701b678974d0b1a509dae7539cd
maximetacquet <maxime.tacquet@student.vives.be>
2025-12-28 16:50:28 +0100

    lid klasse aangemaakt in lid.py
    bekijkt telkens of de waarden van de variabelen none zijn of niet. Indien de variabelen voornaam, achternaam en afdeling niet None zijn worden ze geformateerd als Titel (1ste letter = hoofdletter, de rest = kleine letter)

--- model/lid.py
+++ model/lid.py
@@ -5,3 +5,25 @@ Created on Sun Dec 28 16:09:52 2025
 @author: maxime
 """
 
+class Lid:
+    def __init__(self, id: int=0, voornaam:str="", achternaam:str="", afdeling:str=""):
+        if id is None:
+            self.id=0
+        else:
+            self.id=id
+        if voornaam is None:
+            self.voornaam=""
+        else:
+            self.voornaam=voornaam.title()
+        if achternaam is None:
+            self.achternaam=""
+        else:
+            self.achternaam=achternaam.title()
+        if afdeling is None:
+            self.afdeling=""
+        else:
+            self.afdeling= afdeling.title()
+    
+    def __str__(self):
+        return f"{self.voornaam} {self.achternaam} {self.afdeling}"
+  
