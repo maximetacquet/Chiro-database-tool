7896840d1290c6b2b965c9c8bf685102285fcc2f
maximetacquet <maxime.tacquet@student.vives.be>
2025-12-29 01:05:02 +0100

    Modellen uitgebreid voor afdelingen (2 tabellen)
    Lid-model aangepast: gebruikt nu afdeling_id i.p.v. afdeling-tekst
    Nieuw Afdeling-model + mapping toegevoegd (id 1–6 → afdelingsnaam)

--- model/lid.py
+++ model/lid.py
@@ -5,30 +5,44 @@ Created on Sun Dec 28 16:09:52 2025
 @author: maxime
 """
 
-Afdelingen = ["Pimpel","Speelclub","Rakwi","Tito","Keti","Aspi"]
+from model.afdeling import Afdelingen
+
 
 class Lid:
-    def __init__(self, id: int=0, voornaam:str="", achternaam:str="", afdeling:str=""):
+    def __init__(self, id: int = 0, voornaam: str = "", achternaam: str = "", afdeling_id: int = 0):
         if id is None:
-            self.id=0
+            self.id = 0
         else:
-            self.id=id
+            self.id = id
+
         if voornaam is None:
-            self.voornaam=""
+            self.voornaam = ""
         else:
-            self.voornaam=voornaam.title()
+            self.voornaam = voornaam.title()
+
         if achternaam is None:
-            self.achternaam=""
+            self.achternaam = ""
         else:
-            self.achternaam=achternaam.title()
-        if afdeling is None:
-            self.afdeling=""
-        elif afdeling.title() not in Afdelingen:
-            self.afdeling=""
-            print(f"Afdeling {afdeling} is ongeldig. Kies uit: {', '.join(Afdelingen)}")
-            print("Als afdeling werd er ipv een foute afdeling niets ingegeven")
+            self.achternaam = achternaam.title()
+
+        if afdeling_id is None:
+            self.afdeling_id = 0
         else:
-            self.afdeling= afdeling.title()
-    
+            try:
+                afdeling_id = int(afdeling_id)
+            except (TypeError, ValueError):
+                afdeling_id = 0
+
+            if afdeling_id not in Afdelingen:
+                self.afdeling_id = 0
+                print(f"AfdelingID {afdeling_id} is ongeldig. Kies uit: {', '.join(str(k) for k in Afdelingen.keys())}")
+                print("Als afdelingID werd er ipv een foute afdelingID niets ingegeven")
+            else:
+                self.afdeling_id = afdeling_id
+
+    def afdeling(self) -> str:
+        
+        return Afdelingen.get(self.afdeling_id, "")
+
     def __str__(self):
         return f"{self.voornaam} {self.achternaam} {self.afdeling}"
